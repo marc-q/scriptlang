e524961ce5467dc07c870ff25540d43f6ec13aad
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2016-12-31 17:34:48 +0100

    Update copyright year.

--- scriptlang.c
+++ scriptlang.c
@@ -1,4 +1,4 @@
-/* Copyright 2015 - 2016 Marc Volker Dickmann */
+/* Copyright 2015 - 2017 Marc Volker Dickmann */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -723,7 +723,7 @@ static int read_file (char* filename)
 
 int main (int argc, char* argv[])
 {
-	printf ("Scriptlang v. %s (C) 2015 - 2016 Marc Volker Dickmann\n\n", P_VERSION);
+	printf ("Scriptlang v. %s (C) 2015 - 2017 Marc Volker Dickmann\n\n", P_VERSION);
 	
 	srand (time (NULL));
 	
